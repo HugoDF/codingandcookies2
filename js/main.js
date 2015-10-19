ee2c74a74fed33f1b4d28c1df9e3052524c1dee5
HugoDF <hdifrancesco@gmail.com>
2015-10-19 18:53:52 +0100

    do all the things

--- js/main.js
+++ js/main.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
   $('.mobile-menu-toggle').on('click', function(){
     $('.nav-list').toggleClass('open');
   });
+  $('.listing').on('click', function(){
+    $('.listing').removeClass('open');
+    $(this).addClass('open');
+  });
+
 });
+
+
+$('body,html').scrollTop(5000);
